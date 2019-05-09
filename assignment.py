9d35f519f50aedf57f0d1dbabdb59bf046de461a
Evangelos Rokkos <erokkos@deepmed.io>
2019-05-09 16:25:14 +0300

    Upload file to correct folder

--- assignment.py
+++ assignment.py
@@ -2,11 +2,15 @@
 
 
 """
+import os
 from flask import Flask
 from routes.routes import init_routes
 
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = os.path.join(os.path.dirname(__file__), 'uploads')
+app.secret_key = os.urandom(24)
+
 init_routes(app)
 
 if __name__ == '__main__':
