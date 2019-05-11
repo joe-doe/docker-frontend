544f2ba34330523b7d2f2c7675c7ee05258dc288
evangelos.r <evangelos.r@tutanota.com>
2019-05-11 13:11:59 +0300

    create image builder object

--- assignment.py
+++ assignment.py
@@ -5,13 +5,16 @@
 import os
 from flask import Flask
 from routes.routes import init_routes
+from build_image.build_image import BuildImage
 
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = os.path.join(os.path.dirname(__file__), 'uploads')
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'uploads')
 app.secret_key = os.urandom(24)
 
-init_routes(app)
+image_obj = BuildImage()
+
+init_routes(app, image_obj)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
