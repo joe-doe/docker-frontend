bf6b655be99a7dbf4c07e7658df00f06c0a11c7f
evangelos.r <evangelos.r@tutanota.com>
2019-05-11 13:12:53 +0300

    Image builder class

--- build_image/build_image.py
+++ build_image/build_image.py
@@ -1,4 +1,12 @@
-"""
+from docker_client.client import client
 
 
-"""
+class BuildImage(object):
+    def __init__(self):
+        self.image = None
+        self.image_build_output = None
+
+    def build(self, dockerfile_path):
+        self.image, self.image_build_output = \
+            client.images.build(path=dockerfile_path)
+        return self.image, self.image_build_output
