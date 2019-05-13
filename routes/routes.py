bd8502c89a397143ac22b2f14341808ce8a8bd4d
evangelos.r <evangelos.r@tutanota.com>
2019-05-13 14:32:03 +0300

    provide 'select-image' and 'use-image' routes in order to initialize image object if user didn't use the build route.

--- routes/routes.py
+++ routes/routes.py
@@ -4,10 +4,15 @@ This module contains all routes for Flask application
 import os
 from flask import render_template, url_for, request, flash, redirect
 from werkzeug.utils import secure_filename
-from docker.errors import BuildError, APIError
+from docker.errors import (
+    BuildError,
+    APIError,
+    ContainerError,
+    ImageNotFound
+)
 
 
-def init_routes(app, image_obj):
+def init_routes(app, image_handler, containers_handler):
     @app.route("/")
     def index():
         return render_template('index.html')
@@ -17,16 +22,18 @@ def init_routes(app, image_obj):
         dockerfile = os.path.join(app.config['UPLOAD_FOLDER'])
 
         try:
-            image, image_build_output = image_obj.build(dockerfile)
-        except BuildError as build_error:
-            flash("There was an error during the build: "+build_error.msg)
+            image, image_build_output = image_handler.build(dockerfile)
+        except BuildError:
+            flash("There was an error during the build.")
+            print("error_code: 1001")
             return redirect(url_for('index'))
-        except APIError as api_error:
-            flash("The docker API server returned error: "+api_error.explanation)
+        except APIError:
+            flash("The docker API server returned error.")
+            print("error_code: 1002")
             return redirect(url_for('index'))
         except TypeError as type_error:
             flash("Invalid dockerfile path")
-            print(type_error)
+            print("error_code: 1003")
             return redirect(url_for('index'))
 
         image_build_response = []
@@ -63,9 +70,52 @@ def init_routes(app, image_obj):
 
         return render_template('upload.html')
 
+    @app.route("/select-image")
+    def select_image():
+        image_list = image_handler.get_image_list()
+        return render_template('select_image.html', image_list=image_list)
+
+    @app.route("/use-image", methods=['POST'])
+    def use_image():
+        try:
+            image_id = request.form['image_id']
+        except KeyError as key_error:
+            flash('Wrong image id:' + str(key_error))
+            return redirect(url_for('select_image'))
+
+        try:
+            image_handler.set_image(image_id)
+        except ImageNotFound as image_not_found_error:
+            flash("Image not found: " + image_not_found_error.explanation)
+            return redirect(url_for('index'))
+        except APIError as api_error:
+            flash("The docker API server returned error: " + api_error.explanation)
+            return redirect(url_for('index'))
+
+        return redirect(url_for('start_container'))
+
     @app.route("/start-container")
     def start_container():
-        return render_template('start_container.html')
+        image = image_handler.get_image()
+
+        if not image:
+            flash("Docker image is not available yet. Run build image first or select form list")
+            return redirect(url_for('select_image'))
+
+        try:
+            containers_handler.start_new(image)
+        except ContainerError as container_error:
+            flash("There was an error during the new container: " + container_error.stderr)
+            return redirect(url_for('index'))
+        except ImageNotFound as image_not_found_error:
+            flash("Image not found: " + image_not_found_error.explanation)
+            return redirect(url_for('index'))
+        except APIError as api_error:
+            flash("The docker API server returned error: " + api_error.explanation)
+            return redirect(url_for('index'))
+
+        flash("Container started successfully")
+        return redirect(url_for('index'))
 
     @app.route("/monitoring")
     def monitoring():
