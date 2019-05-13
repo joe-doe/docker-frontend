0ad68a02ed48adf450a118c256d6a8a7e7928acf
evangelos.r <evangelos.r@tutanota.com>
2019-05-13 15:15:17 +0300

    add get containers status to containers handler

--- start_container/start_container.py
+++ start_container/start_container.py
@@ -3,8 +3,16 @@ from docker_client.client import client
 
 class ContainersHandler(object):
     def __init__(self):
-        self.containers_list = []
+        pass
 
     def start_new(self, image):
-        new_container = client.containers.run(image, detach=True)
-        self.containers_list.append(new_container)
+        client.containers.run(image, detach=True)
+
+    def get_containers_status(self):
+        status = {}
+        all_containers = client.containers.list(all=True)
+
+        for container in all_containers:
+            status[container.name] = container.status
+
+        return status
