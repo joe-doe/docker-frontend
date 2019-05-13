26665b66ba73b1a61c62ce56232b1a8fc6bf6dc1
evangelos.r <evangelos.r@tutanota.com>
2019-05-13 17:12:34 +0300

    subtract functionality to get all containers in new method

--- start_container/start_container.py
+++ start_container/start_container.py
@@ -8,9 +8,12 @@ class ContainersHandler(object):
     def start_new(self, image):
         client.containers.run(image, detach=True)
 
+    def get_containers(self, all_containers):
+        return client.containers.list(all=all_containers)
+
     def get_containers_status(self):
         status = {}
-        all_containers = client.containers.list(all=True)
+        all_containers = self.get_containers(all_containers=True)
 
         for container in all_containers:
             status[container.name] = container.status
