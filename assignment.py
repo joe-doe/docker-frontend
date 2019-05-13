35b56bd10d40c53a7e3411aeb414bca24c87c4cf
evangelos.r <evangelos.r@tutanota.com>
2019-05-13 17:10:59 +0300

    create monitoring object

--- assignment.py
+++ assignment.py
@@ -7,6 +7,7 @@ from flask import Flask
 from routes.routes import init_routes
 from build_image.build_image import ImageHandler
 from start_container.start_container import ContainersHandler
+from monitoring.monitoring import Monitor
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'uploads')
@@ -14,8 +15,9 @@ app.secret_key = os.urandom(24)
 
 image_builder = ImageHandler()
 containers_handler = ContainersHandler()
+monitor = Monitor(containers_handler)
 
-init_routes(app, image_builder, containers_handler)
+init_routes(app, image_builder, containers_handler, monitor)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
