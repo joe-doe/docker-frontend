b9672f9d9e0ba740daec876d71522bff72613075
evangelos.r <evangelos.r@tutanota.com>
2019-05-14 14:33:24 +0300

    Start process in background and for every running container start a new process to stream its log to mongodb

--- start_container/start_container.py
+++ start_container/start_container.py
@@ -1,9 +1,31 @@
 from docker_client.client import client
+import multiprocessing as mp
+import time
 
 
 class ContainersHandler(object):
-    def __init__(self):
-        pass
+    def __init__(self, db):
+        self.db = db
+        self.running_containers = mp.Manager().list()
+
+        update_running = mp.Process(target=self.update_running_container_list)
+        update_running.start()
+
+    def update_running_container_list(self):
+        while True:
+            currently_running_containers = self.get_containers(all_containers=False)
+            for running_container in currently_running_containers:
+                if running_container not in self.running_containers:
+                    self.running_containers.append(running_container)
+
+                    p = mp.Process(
+                        target=self.update_log,
+                        args=(running_container, )
+                    )
+                    p.daemon = True
+                    p.start()
+
+            time.sleep(1)
 
     def start_new(self, image):
         client.containers.run(image, detach=True)
@@ -19,3 +41,18 @@ class ContainersHandler(object):
             status[container.name] = container.status
 
         return status
+
+    def update_log(self, container):
+        print("Watching: {}".format(container.name))
+        log_generator = container.logs(stream=True, follow=True)
+        while container.status == 'running':
+            try:
+                entry = next(log_generator)
+            except StopIteration:
+                self.running_containers.remove(container)
+                print(container.name+" log stream STOPPED")
+                break
+            self.db.entry.insert({container.name: str(entry)})
+            print(container.name, entry)
+            time.sleep(0.5)
+        self.running_containers.remove(container)
