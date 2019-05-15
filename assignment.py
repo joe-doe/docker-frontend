b47a12d73cf8ed19b808e17e2091ac3802ef1956
evangelos.r <evangelos.r@tutanota.com>
2019-05-15 13:33:45 +0300

    Support websockets for log streaming

--- assignment.py
+++ assignment.py
@@ -9,17 +9,22 @@ from build_image.build_image import ImageHandler
 from start_container.start_container import ContainersHandler
 from monitoring.monitoring import Monitor
 from database.database import get_db
+from flask_socketio import SocketIO
+from websocks.websocks import init_websockets
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'uploads')
 app.secret_key = os.urandom(24)
 
+socketio = SocketIO(app)
 db = get_db()
 image_builder = ImageHandler()
 containers_handler = ContainersHandler(db)
 monitor = Monitor(containers_handler)
 
 init_routes(app, image_builder, containers_handler, monitor)
+init_websockets(socketio, db)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    socketio.run(app)
