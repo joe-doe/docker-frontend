3d54a709b69cb5f8c3d8ee6280ebad6f8841eb52
evangelos.r <evangelos.r@tutanota.com>
2019-05-15 13:34:55 +0300

    Create capped collection on mongodb in order to use a <<tail>> cursor when retrieving documents

--- database/database.py
+++ database/database.py
@@ -1,5 +1,11 @@
+from pymongo.errors import CollectionInvalid
+
 def get_db():
     from pymongo import MongoClient
     client = MongoClient(host='0.0.0.0:27017', connect=False)
     db = client.assignment
+    try:
+        db.create_collection('entry', capped=True, size=20000000000)
+    except CollectionInvalid:
+        print("Collection already exist. Moving on")
     return db
