616ea2e8e562b827184254599736facc4e854781
evangelos.r <evangelos.r@tutanota.com>
2019-05-16 09:19:57 +0300

    Comment out mongodb-websockets realtime feeding

--- assignment.py
+++ assignment.py
@@ -1,30 +1,38 @@
 """
-
-
+Module that contains main application
+and the needed initializations
 """
 import os
 from flask import Flask
 from routes.routes import init_routes
 from build_image.build_image import ImageHandler
-from start_container.start_container import ContainersHandler
+from containers_handler.containers_handler import ContainersHandler
 from monitoring.monitoring import Monitor
-from database.database import get_db
-from flask_socketio import SocketIO
-from websocks.websocks import init_websockets
+# EXPERIMENTAL #
+# from database.database import get_db
+# from flask_socketio import SocketIO
+# from websocks.websocks import init_websockets
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'uploads')
 app.secret_key = os.urandom(24)
 
-socketio = SocketIO(app)
-db = get_db()
+# EXPERIMENTAL #
+# socketio = SocketIO(app)
+# db = get_db()
+# containers_handler = ContainersHandler(db)
+containers_handler = ContainersHandler()
+
 image_builder = ImageHandler()
-containers_handler = ContainersHandler(db)
 monitor = Monitor(containers_handler)
 
 init_routes(app, image_builder, containers_handler, monitor)
-init_websockets(socketio, db)
+
+# EXPERIMENTAL #
+# init_websockets(socketio, db)
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    # EXPERIMENTAL #
+    # socketio.run(app)
+    app.run(host='0.0.0.0')
